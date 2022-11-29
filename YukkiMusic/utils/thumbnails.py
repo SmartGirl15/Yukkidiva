3f1c7b057388e9246641d7f22c9ad748aa5ba9db
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2022-11-29 18:55:25 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -101,20 +101,12 @@ async def gen_thumb(videoid):
             stroke_width=2,
             stroke_fill="black",
             font=font2,
-        ) 
-        draw.text(
-            (100, 600),
-            "PLAYING NOW!",
-            fill="white",
-            stroke_width=2,
-            stroke_fill="black",
-            font=font2,
-        )
+        )        
         for line in para:            
             if j == 1:
                 j += 1
                 draw.text(
-                    (50, 790),
+                    (50, 690),
                     f"{line}",
                     fill="white",
                     stroke_width=1,
@@ -124,7 +116,7 @@ async def gen_thumb(videoid):
             if j == 0:
                 j += 1
                 draw.text(
-                    (50, 730),
+                    (50, 630),
                     f"{line}",
                     fill="white",
                     stroke_width=1,
@@ -133,7 +125,7 @@ async def gen_thumb(videoid):
                 )
 
         draw.text(
-            (100, 890),
+            (100, 790),
             f"Views : {views[:23]}",
             (255, 255, 255),
             font=jokerman,
@@ -141,7 +133,7 @@ async def gen_thumb(videoid):
             stroke_fill="black",
         )
         draw.text(
-            (100, 940),
+            (100, 840),
             f"Duration : {duration[:23]} Mins",
             (255, 255, 255),
             font=jokerman,
@@ -149,7 +141,7 @@ async def gen_thumb(videoid):
             stroke_fill="black",
         )
         draw.text(
-            (100, 990),
+            (100, 890),
             f"Channel : {channel}",
             (255, 255, 255),
             font=jokerman,
