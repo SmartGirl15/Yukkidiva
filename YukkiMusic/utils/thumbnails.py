6da69633fb1dc0c74059d0b9118a6b409a983a13
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2022-11-29 13:07:05 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -21,8 +21,8 @@ from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
 
 def changeImageSize(maxWidth, maxHeight, image):
-    widthRatio = maxWidth / image.size[0]
-    heightRatio = maxHeight / image.size[1]
+    heightRatio = maxWidth / image.size[0]
+    widthRatio = maxHeight / image.size[1]
     newWidth = int(widthRatio * image.size[0])
     newHeight = int(heightRatio * image.size[1])
     newImage = image.resize((newWidth, newHeight))
